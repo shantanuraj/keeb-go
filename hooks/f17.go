1bd33cbbb2f72e586a481e17d1aab71eed7f448f
Shantanu Raj <s@sraj.me>
2021-11-13 16:22:52 +0100

    Implement f17

--- hooks/f17.go
+++ hooks/f17.go
@@ -1,7 +1,19 @@
 package hooks
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func HandleF17() {
-	fmt.Println("F17")
+	playSpotifyPlaylist(os.Getenv("KEEB_PLAYLIST"))
+}
+
+func playSpotifyPlaylist(playlist string) (bool, error) {
+	fmt.Println("Playing:", playlist)
+	var err error
+	if _, err = tell(Spotify, fmt.Sprintf("play track \"%s\"", playlist)); err != nil {
+		return false, err
+	}
+	return true, nil
 }
